e7db12e6fb931f17322248ed711f24f746334bdd
Giacomo Giudice <giacomo.giudice@mail.com>
2016-09-17 11:14:03 +0200

    feat(map): add marker grid

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+'use strict';
 (function app() {
   var GEOLOCATION_NOT_SUPPORTED = "Geolocation is not supported by this browser.";
   var CONTAINER_ID = "container";
   var SEARCHBOX_ID = "search-box";
   var BOUNDS_CHANGED_EVENT = "bounds_changed";
   var PLACES_CHANGED_EVENT = "places_changed";
-  var POINTS_DENISITY = 0.001;
+  var POINT_SPACING = 50; // pixels in between points
   var POINTS_RADIUS = 3;
+
+  var map = {};
 
   var initApp = () => {
     return new Promise((resolveCoords) => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(resolveCoords);
       } else {
-        x.innerHTML = GEOLOCATION_NOT_SUPPORTED;
+        // x.innerHTML = GEOLOCATION_NOT_SUPPORTED;
       }
     });
   };
@@ -32,7 +35,7 @@
     });
   }
 
-  var initSearch = map => {
+  var initSearch = (map) => {
     var input = document.getElementById(SEARCHBOX_ID);
     var searchBox = new google.maps.places.SearchBox(input);
     var marker = { setMap: function () {} };
@@ -57,7 +60,7 @@
         var center = new google.maps.LatLng(
           resultPlaceCoords.latitude,
           resultPlaceCoords.longitude
-        );
+          );
 
         map.panTo(center);
         marker = new google.maps.Marker({
@@ -65,8 +68,27 @@
           title: place.name,
           position: place.geometry.location
         });
+        // here be new stuff
 
-        onSearchComplete(resultPlaceCoords);
+        var southWest = map.getBounds().getSouthWest();
+        var northEast = map.getBounds().getNorthEast();
+
+        var width = document.getElementById(CONTAINER_ID).offsetWidth;
+        var height = document.getElementById(CONTAINER_ID).offsetHeight;
+
+        var n_width = Math.round(width/POINT_SPACING);
+        var n_height = Math.round(height/POINT_SPACING);
+
+        var delta_w = (northEast.lng() - southWest.lng())/n_width;
+        var delta_h = (northEast.lat() - southWest.lat())/n_height;
+        var grid = [];
+        for (var i = 0; i < n_height; i++) {
+          for (var j = 0; j < n_width; j++) {
+            grid.push({latitude: southWest.lat() + i*delta_h, longitude: southWest.lng() + j*delta_w});
+          }
+        }        
+
+        onSearchComplete(grid);
       });
     })
 
@@ -74,36 +96,31 @@
   };
 
   var onCenterPointCoordsRetrieved = (coords) => {
-    var locationGrid = [coords];
-    for (var i = -POINTS_RADIUS; i < POINTS_RADIUS; i++) {
-      for (var k = -POINTS_RADIUS; k < POINTS_RADIUS; k++) {
-
-      }
-    }
-
+    
     return new Promise(geoLocationGridResolver => {
       geoLocationGridResolver({ map: map, locationGrid: locationGrid })
     });
   }
 
   var processGeoLocationPointGrid = (gridData) => {
-    for (var i = 0; i < gridData.locationGrid.length; i++) {
-      var resultPlaceCoords = gridData.locationGrid[i];
+    for (var i = 0; i < gridData.length; i++) {
+      var resultPlaceCoords = gridData[i];
       new google.maps.Marker({
-        map: gridData.map,
+        map: map,
         title: "Point" + i,
         position: new google.maps.LatLng(
           resultPlaceCoords.latitude,
           resultPlaceCoords.longitude
-        ),
+          ),
       });
-
     }
   }
 
   initApp()
-    .then(initMap)
-    .then(initSearch)
-    .then(onCenterPointCoordsRetrieved)
-    .then(processGeoLocationPointGrid)
+  .then(initMap)
+  .then(initSearch)
+  // .then(onCenterPointCoordsRetrieved)
+  .then(processGeoLocationPointGrid)
+
+
 })();
