3e6d3a0a09f4398cbff7cf74c526d0bcd05bcd66
Mateusz S <mateusz.sidorski@gmail.com>
2016-09-18 02:48:29 +0200

    feat(homegate): client data fetching

--- homeGateClient.js
+++ homeGateClient.js
@@ -1,23 +1,30 @@
 var homeGateClient = (config) => {
 
   return {
-    searchWithinArea: (location) => { 
-      return new Promise((success, fail) => {       
-         $.ajax({
+    searchWithinArea: (longitude, latitude) => {
+      return new Promise((success, fail) => {
+        $.ajax({
           method: "GET",
-          url: "https://api.tamedia.cloud/homegate/v1c/rs/real-estates?lan=de&cht=rentflat&nrs=300&wrg=2000&nby=" + encodeURIComponent(location.longitude + "," + location.latitude),          
+          url: "https://api.tamedia.cloud/homegate/v1c/rs/real-estates?lan=de&cht=rentflat&nrs=300&wrg=2000&nby=" +
+            encodeURIComponent(longitude + "," + latitude),
           crossDomain: true,
           beforeSend: function (xhr) {
-            debugger
             xhr.setRequestHeader('apiKey', "1a59a7422e2d4936b02e9730d9f579b8");
             xhr.setRequestHeader('Accept', 'application/json')
             xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
           },
           success: data => {
-            debugger
+            success(data.items.map(x => {
+                return {
+                    title: x.title,
+                    description: x.description,                  
+                    url: 'https://homegate.ch/rent/' + x.advId,
+                    picture: x.picFilename1
+                }
+            }));
           },
           error: (e) => {
-              console.log(e.responseJSON)
+            console.log(e.responseJSON)
             fail(e);
           }
         });
