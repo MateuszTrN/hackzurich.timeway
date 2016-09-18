1fc8ecdf68360b8c3e59f81d1935cc0c9763ca91
Mateusz S <mateusz.sidorski@gmail.com>
2016-09-18 02:26:13 +0200

    feat(homegate-client): simple client for homegate

---
+++ homeGateClient.js
@@ -0,0 +1,27 @@
+var homeGateClient = (config) => {
+
+  return {
+    searchWithinArea: (southWestLocation, northEastLocation) => {
+      return new Promise((success, fail) => {       
+         $.ajax({
+          method: "GET",
+          url: "https://api.tamedia.cloud/homegate/v1c/rs/real-estates?lan=de&cht=rentflat&nrs=300",          
+          crossDomain: true,
+          beforeSend: function (xhr) {
+            debugger
+            xhr.setRequestHeader('apiKey', "1a59a7422e2d4936b02e9730d9f579b8");
+            xhr.setRequestHeader('Accept', 'application/json')
+            xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+          },
+          success: data => {
+            debugger
+          },
+          error: (e) => {
+              console.log(e.responseJSON)
+            fail(e);
+          }
+        });
+      });
+    }
+  }
+}
